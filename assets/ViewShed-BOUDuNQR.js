96c1b371b1068c8cdffa65bc1ea690c10d6659eb
mxfh <mxfh@users.noreply.github.com>
2026-02-09 14:10:34 +0000

    deploy: cismet/carma@ed0e8dfd11f8b0492e1faec0308d58b52db200bf

--- assets/ViewShed-BOUDuNQR.js
+++ assets/ViewShed-BOUDuNQR.js
@@ -1,4 +1,4 @@
-import{d as w,e as p,f as i,g as y,P as z,h as _,i as D,k as A,l as g,E as L,m as E,T,M as b,r as c,W as R,j as d,V as U,c as B}from"./index-CW5dlatL.js";import{u as O}from"./useTileset-csJLfML9.js";import{B as x}from"./button-CZubMn6z.js";import"./index-DMWlXRq0.js";const V=`
+import{d as w,e as p,f as i,g as y,P as z,h as _,i as D,k as A,l as g,E as L,m as E,T,M as b,r as c,W as R,j as d,V as U,c as B}from"./index-aJhKk1Yr.js";import{u as O}from"./useTileset-COPdpTyc.js";import{B as x}from"./button-B8-jMv_T.js";import"./index-DNemmbV1.js";const V=`
 #define USE_NORMAL_SHADING
 uniform float view_distance; // Maximum distance for shadow effect
 uniform vec3 viewArea_color; // Color for visible areas

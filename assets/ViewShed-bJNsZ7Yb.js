efd7609d048c3ab1a3671cb04ec977c25fe5060e
mxfh <mxfh@users.noreply.github.com>
2026-06-13 22:17:53 +0000

    deploy: cismet/carma@816c03d42ee2c72749fb887222867a4d844bed6f

--- assets/ViewShed-bJNsZ7Yb.js
+++ assets/ViewShed-bJNsZ7Yb.js
@@ -1,4 +1,4 @@
-import{C as p,a as i,b as _,d as g,e as y,T as z,M as D,r as c,j as d,c as A}from"./index-BQ-nuF_n.js";import{c as w,d as L,P as E,e as b,f as T,E as R,W as U,V as B}from"./tileset3d-M1-2JzAC.js";import{u as O}from"./useTileset-CxGz6Jxk.js";import{B as x}from"./button-jJ1oGvJi.js";import"./index-Xt9OB04S.js";import"./Compact-BoAay_OQ.js";const V=`
+import{C as p,a as i,b as _,d as g,e as y,T as z,M as D,r as c,j as d,c as A}from"./index-ALXAwEWt.js";import{c as w,d as L,P as E,e as b,f as T,E as R,W as U,V as B}from"./tileset3d-nr4W-Or3.js";import{u as O}from"./useTileset-DbXVOhTP.js";import{B as x}from"./button-DJ3DyfFB.js";import"./index-R6ye6IS_.js";import"./Compact-CK0pWMX9.js";const V=`
 #define USE_NORMAL_SHADING
 uniform float view_distance; // Maximum distance for shadow effect
 uniform vec3 viewArea_color; // Color for visible areas

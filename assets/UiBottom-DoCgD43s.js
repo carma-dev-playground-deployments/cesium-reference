5d38bec5b78c9955e88bc2492a5c25e750078f99
mxfh <mxfh@users.noreply.github.com>
2026-09-18 00:20:41 +0000

    deploy: cismet/carma@8321a13e34d844b05b9aa3ff7fd76e83b857b5fa

---
+++ assets/UiBottom-DoCgD43s.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BbPX9y1q.js";const s=({children:t})=>o.jsx("div",{style:{position:"absolute",bottom:10,right:10,left:10,padding:"10px",background:"rgba(255, 255, 255, 0.85)"},children:t});export{s as U};
--- assets/ViewShed-DP7rvvss.js
+++ assets/ViewShed-DP7rvvss.js
@@ -1,4 +1,4 @@
-import{C as p,a as i,b as _,d as g,e as y,T as z,M as D,r as c,j as d,c as A}from"./index-Bp4vJmTr.js";import{c as w,d as L,P as E,e as b,f as T,E as R,W as U,V as B}from"./tileset3d-DMmVVPLq.js";import{u as O}from"./useTileset-BdVhpJWj.js";import{B as x}from"./button-e3YHulAE.js";import"./index-D5YjY-ug.js";import"./Compact-DmN68MmR.js";const V=`
+import{C as p,a as i,b as _,d as g,e as y,T as z,M as D,r as c,j as d,c as A}from"./index-BbPX9y1q.js";import{c as w,d as L,P as E,e as b,f as T,E as R,W as U,V as B}from"./tileset3d-C0tqpy1z.js";import{u as O}from"./useTileset-DHlIOsaF.js";import{B as x}from"./button-Dek7SGbS.js";import"./index-B9c8gbmh.js";import"./Compact-Ckx9iMTt.js";const V=`
 #define USE_NORMAL_SHADING
 uniform float view_distance; // Maximum distance for shadow effect
 uniform vec3 viewArea_color; // Color for visible areas
